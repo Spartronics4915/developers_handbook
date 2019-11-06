5120fcbc14de98fc10a7e94751126a0b89a656cd
j-james <jj@j-james.me>
2019-11-06 08:22:31 -0800

    Move solutions into reference_lessons

--- robot_programming/robot_lessons/lesson3/src/main/java/com/spartronics4915/learnyouarobot/Lesson.java
+++ robot_programming/robot_lessons/lesson3/src/main/java/com/spartronics4915/learnyouarobot/Lesson.java
@@ -12,38 +12,27 @@ public class Lesson extends CommandBase {
     // Creates a new ShootCommand (it's called Lesson here).
     public Lesson() {
         // Subsystems are set here
-        mShooterSubsystem = ShooterSubsystem.getInstance();
-        count = 0;
     }
 
     public void initialize() {
         // Code you want to run upon start
-        mShooterSubsystem.stop();
     }
 
     public void execute() {
         // Code you want to run over and over
-        mShooterSubsystem.shootBall(0.5);
     }
 
     public void end(boolean interrupted) {
         if (interrupted) {
             // Code you want to run if a command is interrupted
-            mShooterSubsystem.stop();
         }
         else {
             // Code you want to run if a command finishes normally
-            mShooterSubsystem.stop();
         }
     }
 
     public boolean isFinished() {
         // What is the finish condition?
-        if (count == 200)
-            return true;
-        else
-            count++;
-        return false;
     }
 
     public void outputTelemetry() {
